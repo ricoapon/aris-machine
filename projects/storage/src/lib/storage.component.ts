4ed6c455f53ff7e16f9c0e6f39af99085a0b213f
Rico Apon <rico.apon@gmail.com>
2023-05-30 19:29:21 +0200

    Generate storage module

---
+++ projects/storage/src/lib/storage.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-storage',
+  template: `
+    <p>
+      storage works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class StorageComponent {
+
+}
---
+++ projects/storage/src/lib/storage.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { StorageComponent } from './storage.component';
+
+
+
+@NgModule({
+  declarations: [
+    StorageComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    StorageComponent
+  ]
+})
+export class StorageModule { }
