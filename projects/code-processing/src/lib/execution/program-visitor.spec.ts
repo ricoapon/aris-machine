d9c9724c2d3aae2af0fa5c1439bc93b996dd7609
Rico Apon <rico.apon@gmail.com>
2023-06-01 20:47:09 +0200

    Implement program executor based on code

---
+++ projects/code-processing/src/lib/execution/program-visitor.spec.ts
@@ -0,0 +1,79 @@
+import {Level, Stage} from "../level";
+import {ProgramVisitor} from "./program-visitor";
+
+// Note: level solutions are tested, which means that this code is already heavily tested (at least happy flows).
+// This means we need to focus on error situations or very specific situations (like testing a difficult to program keyword).
+describe('ProgramVisitor', () => {
+  const DEFAULT_LEVEL: Level = {
+    stage: Stage.MAIN,
+    id: 0,
+    title: '',
+    input: [],
+    expectedOut: [],
+    nrOfMemorySlots: 100,
+    maxCodeLengthScoreForStar: 0,
+    maxRunLengthScoreForStar: 0,
+    description: ''
+  }
+
+  it('infinite loops are cut off', () => {
+    // Given
+    const level = {...DEFAULT_LEVEL, input: [1], expectedOut: [0]}
+    const code =
+      'move input to 0\n' +
+      'loop {\n' +
+      '  move 0 to 1\n' +
+      '  move 1 to 0\n' +
+      '}'
+
+    // When
+    const result = new ProgramVisitor().run(level, code)
+
+    // Then
+    expect(result.finishedWithError).toBeTrue()
+    expect(result.machineGUIActions.pop()).toEqual({error: 'Infinite loop reached'})
+  })
+
+  it('break only breaks out of the most inner loop', () => {
+    // Given
+    const level = {...DEFAULT_LEVEL, input: [0], expectedOut: [1]}
+    const code =
+      'move input to 0\n' +
+      'loop {\n' +
+      '  loop {\n' +
+      '    break\n' +
+      '    incr 0\n' +
+      '  }\n' +
+      '  incr 0\n' +
+      '  move 0 to output\n' +
+      '}'
+
+    // When
+    const result = new ProgramVisitor().run(level, code)
+
+    // Then
+    expect(result.finishedWithError).toBeFalse()
+    expect(result.machineGUIActions.pop()).toEqual({finished: true})
+  })
+
+  it('continue skips all commands until going back to the for loop again', () => {
+    // Given
+    const level = {...DEFAULT_LEVEL, input: [5], expectedOut: [0]}
+    const code =
+      'move input to 0\n' +
+      'loop {\n' +
+      '  decr 0\n' +
+      '  ifnotzero 0 {\n' +
+      '    continue\n' +
+      '  }\n' +
+      '  move 0 to output\n' +
+      '}'
+
+    // When
+    const result = new ProgramVisitor().run(level, code)
+
+    // Then
+    expect(result.finishedWithError).toBeFalse()
+    expect(result.machineGUIActions.pop()).toEqual({finished: true})
+  })
+});
---
+++ projects/code-processing/src/lib/execution/program-visitor.ts
@@ -0,0 +1,281 @@
+import {ArisVisitor} from "../generated/ArisVisitor";
+import {Machine, MachineResult} from "./machine";
+import {ErrorNode, ParseTree, RuleNode} from "antlr4ts/tree";
+import {TerminalNode} from "antlr4ts/tree/TerminalNode";
+import {
+  AddContext,
+  ArisParser,
+  BreakContext,
+  ContinueContext,
+  CopyContext,
+  DecrementContext,
+  IfNegContext,
+  IfNotZeroContext,
+  IfPosContext,
+  IfZeroContext,
+  IncrementContext,
+  LinesContext,
+  LoopContext,
+  MoveContext,
+  SubtractContext
+} from "../generated/ArisParser";
+import {CharStreams, CommonTokenStream} from "antlr4ts";
+import {ArisLexer} from "../generated/ArisLexer";
+import {Level} from "../level";
+
+enum BreakContinueState {
+  IGNORE,
+  CONTINUE,
+  BREAK,
+}
+
+/**
+ * Executes a program by walking through the code using the visitor pattern.
+ * State will be processed in {@link Machine} and the result will be returned.
+ */
+export class ProgramVisitor implements ArisVisitor<void> {
+  private machine: Machine
+  // If execute break or continue commands, we want to go back to the most recent loop we started executing.
+  // We cannot "go back" in the visitor pattern, so the solution is stop block any execution of code until
+  // we are back in the visitLoop function. Not the prettiest solution, but it works.
+  private breakContinueState: BreakContinueState = BreakContinueState.IGNORE
+
+  private shouldStopExecutionUntilLoopIsReached(): boolean {
+    return this.breakContinueState != BreakContinueState.IGNORE
+  }
+
+  run(level: Level, input: string): MachineResult {
+    this.machine = new Machine(level)
+
+    const inputStream = CharStreams.fromString(input);
+    const lexer = new ArisLexer(inputStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new ArisParser(tokenStream);
+
+    this.visit(parser.program())
+    return this.machine.createMachineResult()
+  }
+
+  visit(tree: ParseTree): void {
+    for (let i = 0; i < tree.childCount; i++) {
+      this.call(tree.getChild(i))
+    }
+    this.machine.checkWinningCondition();
+  }
+
+  visitErrorNode(node: ErrorNode): void {
+  }
+
+  visitTerminal(node: TerminalNode): void {
+  }
+
+  visitMove(ctx: MoveContext) {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    const editorLine = ctx.start.line;
+    if (ctx.INPUT() != undefined && ctx.OUTPUT() != undefined) {
+      this.machine.moveInputToOutput(editorLine)
+    } else if (ctx.INPUT() != undefined) {
+      const memorySlot = +ctx.MEMORY_SLOT(0).text
+      this.machine.moveInputToMemorySlot(memorySlot, editorLine)
+    } else if (ctx.OUTPUT() != undefined) {
+      const memorySlot = +ctx.MEMORY_SLOT(0).text
+      this.machine.moveMemorySlotToOutput(memorySlot, editorLine)
+    } else {
+      const from = +ctx.MEMORY_SLOT(0).text
+      const to = +ctx.MEMORY_SLOT(1).text
+      this.machine.moveMemorySlotToMemorySlot(from, to, editorLine)
+    }
+  }
+
+  visitCopy(ctx: CopyContext) {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    const editorLine = ctx.start.line;
+    const from = +ctx.MEMORY_SLOT(0)
+
+    if (ctx.OUTPUT() != undefined) {
+      this.machine.copyMemorySlotToOutput(from, editorLine)
+    } else {
+      const to = +ctx.MEMORY_SLOT(1)
+      this.machine.copyMemorySlotToMemorySlot(from, to, editorLine)
+    }
+  }
+
+  visitAdd(ctx: AddContext) {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    const editorLine = ctx.start.line;
+    const from = +ctx.MEMORY_SLOT(0)
+    const to = +ctx.MEMORY_SLOT(1)
+    this.machine.addMemorySlotToMemorySlot(from, to, editorLine)
+  }
+
+  visitSubtract(ctx: SubtractContext): void {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    const editorLine = ctx.start.line;
+    const from = +ctx.MEMORY_SLOT(0)
+    const to = +ctx.MEMORY_SLOT(1)
+    this.machine.subtractMemorySlotFromMemorySlot(from, to, editorLine)
+  }
+
+  visitIncrement(ctx: IncrementContext): void {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    const editorLine = ctx.start.line;
+    this.machine.incrementMemorySlot(+ctx.MEMORY_SLOT(), editorLine)
+  }
+
+  visitDecrement(ctx: DecrementContext): void {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    const editorLine = ctx.start.line;
+    this.machine.decrementMemorySlot(+ctx.MEMORY_SLOT(), editorLine)
+  }
+
+  visitLoop(ctx: LoopContext) {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    // To prevent loops to go infinite, we set a max that we will never reach in normal situations.
+    let counter = 0;
+    while (counter < 1000 && this.machine.isRunning()) {
+      this.callLines(ctx.lines())
+      counter++
+
+      if (this.breakContinueState == BreakContinueState.BREAK) {
+        this.breakContinueState = BreakContinueState.IGNORE
+        break;
+      } else if (this.breakContinueState == BreakContinueState.CONTINUE) {
+        this.breakContinueState = BreakContinueState.IGNORE
+      }
+    }
+
+    if (counter == 1000) {
+      this.machine.error('Infinite loop reached')
+    }
+  }
+
+  // noinspection JSUnusedLocalSymbols
+  visitBreak(ctx: BreakContext): void {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    this.breakContinueState = BreakContinueState.BREAK
+  }
+
+  // noinspection JSUnusedLocalSymbols
+  visitContinue(ctx: ContinueContext): void {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    this.breakContinueState = BreakContinueState.CONTINUE
+  }
+
+  visitIfZero(ctx: IfZeroContext) {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    let value
+    if (ctx.INPUT() != undefined) {
+      value = this.machine.getValueOfInputElement()
+    } else {
+      // We know the value of memory slot is undefined here.
+      // @ts-ignore
+      value = this.machine.getValueOfMemorySlot(+ctx.MEMORY_SLOT())
+    }
+
+    if (value == 0) {
+      this.callLines(ctx.lines())
+    }
+  }
+
+  visitIfNotZero(ctx: IfNotZeroContext) {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    let value
+    if (ctx.INPUT() != undefined) {
+      value = this.machine.getValueOfInputElement()
+    } else {
+      // We know the value of memory slot is undefined here.
+      // @ts-ignore
+      value = this.machine.getValueOfMemorySlot(+ctx.MEMORY_SLOT())
+    }
+    if (value != 0) {
+      this.callLines(ctx.lines())
+    }
+  }
+
+  visitIfPos(ctx: IfPosContext): void {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    let value
+    if (ctx.INPUT() != undefined) {
+      value = this.machine.getValueOfInputElement()
+    } else {
+      // We know the value of memory slot is undefined here.
+      // @ts-ignore
+      value = this.machine.getValueOfMemorySlot(+ctx.MEMORY_SLOT())
+    }
+    if (value > 0) {
+      this.callLines(ctx.lines())
+    }
+  }
+
+  visitIfNeg(ctx: IfNegContext): void {
+    if (this.shouldStopExecutionUntilLoopIsReached()) {
+      return
+    }
+
+    let value
+    if (ctx.INPUT() != undefined) {
+      value = this.machine.getValueOfInputElement()
+    } else {
+      // We know the value of memory slot is undefined here.
+      // @ts-ignore
+      value = this.machine.getValueOfMemorySlot(+ctx.MEMORY_SLOT())
+    }
+    if (value < 0) {
+      this.callLines(ctx.lines())
+    }
+  }
+
+  visitChildren(node: RuleNode): void {
+    for (let i = 0; i < node.childCount; i++) {
+      this.call(node.getChild(i))
+    }
+  }
+
+  private callLines(lines: LinesContext) {
+    for (let i = 0; i < lines.line().length; i++) {
+      this.call(lines.line()[i])
+    }
+  }
+
+  private call(tree: ParseTree) {
+    if (this.machine.isRunning()) {
+      tree.accept(this)
+    }
+  }
+}
