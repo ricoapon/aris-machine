c6419fb47aee8e84db1c4d06221d5ac8a5467eef
Rico Apon <rico.apon@gmail.com>
2023-06-05 19:45:08 +0200

    Implement editor documentation

--- projects/main-application/src/app/app.module.ts
+++ projects/main-application/src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { LevelSelectionComponent } from './navigation/level-selection/level-selection.component';
-import { HomeComponent } from './navigation/home/home.component';
-import { DocsComponent } from './navigation/docs/docs.component';
-import { DocumentationComponent } from './documentation/documentation.component';
-import { LanguageComponent } from './documentation/language/language.component';
-import { EditorComponent } from './documentation/editor/editor.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {LevelSelectionComponent} from './navigation/level-selection/level-selection.component';
+import {HomeComponent} from './navigation/home/home.component';
+import {DocsComponent} from './navigation/docs/docs.component';
+import {DocumentationComponent} from './documentation/documentation.component';
+import {LanguageComponent} from './documentation/language/language.component';
+import {EditorComponent} from './documentation/editor/editor.component';
+import {CodeEditorModule} from "code-editor";
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { EditorComponent } from './documentation/editor/editor.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CodeEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/main-application/src/app/documentation/editor/editor.component.ts
+++ projects/main-application/src/app/documentation/editor/editor.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ScenarioEventsBuilder} from "code-editor";
+import {ScenarioEvent} from "../../../../../code-editor/src/lib/demo-code-editor/scenario-events-builder";
 
 @Component({
   selector: 'app-editor',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./editor.component.css', '../shared.css']
 })
 export class EditorComponent {
+  scenarioBuilder(): ScenarioEventsBuilder {
+    return new ScenarioEventsBuilder()
+  }
 
+  scenario1(): ScenarioEvent[] {
+    return this.scenarioBuilder().typeText('loo').openSuggestions().acceptSuggestion().build()
+  }
 }
